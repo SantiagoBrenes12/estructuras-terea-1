86639f1f27434c47e50481f8018a94d110f8b99c
santibq12 <itssanti_@outlook.com>
2023-05-16 23:10:45 -0600

    Initial commit, base password class

---
+++ src/Password.java
@@ -0,0 +1,20 @@
+public class Password {
+    private int longitud = 8;
+    private String contrasena;
+    
+    
+    public Password(){}
+    
+    public Password(int longitud){
+        this.longitud = longitud;
+    }
+    
+    public boolean esFuerte(){
+        return false;
+    }
+    
+    private String generarPassword(){
+        return "";
+    }
+    
+}
